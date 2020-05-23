32bb61e5b1bb26c6059dd5e321ec2b3f00b843cd
shubhamj190 <shubhamjadhav190@gmail.com>
2020-05-23 13:04:10 +0530

    A simple notes web app
    want to implement the  other functionality

---
+++ app.js
@@ -0,0 +1,94 @@
+// my user adds the note, addit to the local storage '
+// function to show notes from localStorage
+showNotes()
+
+// function to add note to the array 
+let btn = document.getElementById('addBtn')
+addBtn.addEventListener('click', function (e) {
+
+    let addTxt = document.getElementById('addTxt')
+    let notes = localStorage.getItem("notes")
+    if (notes == null) {
+        notesobj = []
+    }
+    else {
+        notesobj = JSON.parse(notes)
+    }
+    notesobj.push(addTxt.value)
+    localStorage.setItem("notes", JSON.stringify(notesobj))
+    addTxt.value = ""
+    console.log(notesobj)
+    showNotes()
+})
+// function to show notes from localStorage
+
+function showNotes() {
+    let notes = this.localStorage.getItem("notes")
+    if (notes == null) {
+        notesobj = []
+    }
+    else {
+        notesobj = JSON.parse(notes)
+    }
+    let html = ""
+    notesobj.forEach(function (element,index) {
+        html = html + ` <div class="noteCard my-2 mx-2 card" style="width: 18rem;">
+    <div class="card-body">
+        <h5 class="card-title">Note ${index + 1}</h5>
+        <p class="card-text">${element}</p>
+        <button id="${index}" onclick="deleteNote(this.id)" class="btn btn-danger">Delete Note</button>
+        <button class="btn btn-success id-${index} onclick="completedNotes(this.id)" my-2 mx-2">Completed</button>
+    </div>
+</div>`
+    });
+let noteElm=document.getElementById('notes')
+if(notesobj.length !=0){
+    noteElm.innerHTML=html
+}
+else{
+    noteElm.innerHTML=`Nothing to show use "Add a note" section above to add notes`
+}
+
+
+}
+// function to delete the notes
+function deleteNote(index) {
+    console.log("i an deleting",index)
+
+    // if (notes == null) {
+    //     notesobj = []
+    // }
+    // else {
+    //     notesobj = JSON.parse(notes)
+    // }
+    notesobj.splice(index,1)
+    localStorage.setItem("notes", JSON.stringify(notesobj))
+    showNotes()
+
+}
+
+//function of completed notes
+function completedNotes(index) {
+    console.log("completed task of notes")
+    
+}
+
+//function for search
+let search=document.getElementById('searchTxt')
+search.addEventListener('input',function(e) {
+    let inputVal=search.value
+    let noteCard=document.getElementsByClassName('noteCard')
+    Array.from(noteCard).forEach(function(element) {
+        let cardTxt=element.getElementsByTagName('p')[0].innerText
+        if(cardTxt.includes(inputVal))
+        {
+            element.style.display='block'
+        }
+        else{
+            element.style.display='none'
+
+        }
+        
+    });
+})
+
